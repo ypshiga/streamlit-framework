7e2dbf7e3dc8bd30077c71515440d382c5f79029
Yoichi_rig <yoichishiga@gmail.com>
2021-06-25 16:29:27 -0700

    Update for demo
    added demo for testing streamlit and heroku

---
+++ milestone_app.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# test stock API 
+# Your API key is: IRC9K9I0TMTQZPK7
+import requests
+import pandas as pd
+import numpy as np
+import streamlit as st
+import time
+
+st.title('12 day Milestone Ticker app')
+st.write('Stock data test: IBM')
+
+key = 'IRC9K9I0TMTQZPK7'
+ticker_symbol = 'IBM'
+
+
+# In[ ]:
+
+
+def request_stock_price_hist(symbol, token, sample = False):
+    if sample == False:
+        q_string = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&outputsize=full&apikey={}'
+    else:
+        q_string = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'
+
+    st.write("Retrieving stock price data from Alpha Vantage (This may take a while)...")
+    r = requests.get(q_string.format(symbol, token))
+    st.write("Data has been successfully downloaded...")
+    date = []
+    colnames = list(range(0, 7))
+    df = pd.DataFrame(columns = colnames)
+    st.write("Sorting the retrieved data into a dataframe...")
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+    i_tick = 0
+    df_len = (len( r.json()['Time Series (Daily)'].keys()))
+    for i in r.json()['Time Series (Daily)'].keys():
+        date.append(i)
+        row = pd.DataFrame.from_dict(r.json()['Time Series (Daily)'][i], orient='index').reset_index().T[1:]
+        df = pd.concat([df, row], ignore_index=True)
+        latest_iteration.text('Iteration ' + str((i_tick + 1)))
+        bar.progress((i_tick + 1)/df_len)
+        i_tick+=1
+        if i_tick%500==0:
+            time.sleep(0.1)
+    df.columns = ["open", "high", "low", "close", "adjusted close", "volume", "dividend amount", "split cf"]
+    df['date'] = date
+    return df
+
+
+# In[ ]:
+
+
+'Here we go...'
+
+df = request_stock_price_hist('IBM', key)
+'...all done!'
+
+
+# In[ ]:
+
+
+st.write(df)
+
+
+# In[ ]:
+
+
+
+
