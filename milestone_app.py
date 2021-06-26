00497ce13aba1c06ad5590b54781c356da57a330
Yoichi_rig <yoichishiga@gmail.com>
2021-06-25 19:02:59 -0700

    updates live

--- milestone_app.py
+++ milestone_app.py
@@ -24,14 +24,13 @@ key = os.environ['key']
 # In[ ]:
 
 
-@st.cache(suppress_st_warning=True) 
-def request_stock_price_hist(symbol, token, sample = False):
+def request_stock_price_hist(symbol, token, year_stop, sample = False):
     if sample == False:
         q_string = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&outputsize=full&apikey={}'
     else:
         q_string = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'
 
-    st.write("Retrieving stock price data from Alpha Vantage (This may take a while)...")
+    st.write("Starting stock price data retrieval from Alpha Vantage (This may take a while)...")
     r = requests.get(q_string.format(symbol, token))
     st.write("Data has been successfully downloaded...")
     date = []
@@ -49,9 +48,9 @@ def request_stock_price_hist(symbol, token, sample = False):
         latest_iteration.text('Iteration ' + str((i_tick + 1)))
         bar.progress((i_tick + 1)/df_len)
         i_tick+=1
-        if i_tick%500==0:
-            time.sleep(0.1)
-        if i[0:4]=='2009': # only load data from 2010 onward
+        year_stop = str(int(year_stop)-1)
+        if i[0:4]=='2009' or i[0:4]==str(year_stop): # only load data from 2010 onward
+            bar.progress(100)
             break
     df.columns = ["open", "high", "low", "close", "adjusted close", "volume", "dividend amount", "split cf"]
     df['date'] = date
@@ -62,21 +61,27 @@ def request_stock_price_hist(symbol, token, sample = False):
 
 
 # add user input
-ticker_symbol = st.text_input("Pick a stock ticker label (e.g., IBM):")
-year_select = st.selectbox(
-    'Pick a year:',
-     list(range(2010,2021)))
-month_select = st.selectbox(
-    'Pick a month:',
-     list(range(1,13)))
+ticker_symbol=[]
+year_select=[]
+month_select=[]
+ticker_symbol = st.text_input("Pick a stock ticker label (e.g., IBM, AAPL, GOOGL):")
+year_select = st.selectbox('Pick a year:',list(['Select']) + list(range(2010,2021)))
+month_select = st.selectbox('Pick a month:',list(range(1,13)))
 
 
 # In[ ]:
 
 
-'Here we go...'
-df = request_stock_price_hist(ticker_symbol', key)
-'...all done!'
+if len(ticker_symbol)!=0 and year_select!='Select' and month_select>1:
+    df = request_stock_price_hist(ticker_symbol, key,year_select)
+    df['adjusted close']=pd.to_numeric(df['adjusted close'], errors='coerce')
+    df['date']=pd.to_datetime(df['date'],format='%Y-%m-%d')
+    # plots     
+    date_start = str(year_select) + '-' + str(month_select) + '-' + '01'
+    date_end =  str(year_select) + '-' + str(int(month_select)+1) + '-' + '01'
+    df = df[(df['date'] > pd.to_datetime(date_start)) & (df['date'] < pd.to_datetime(date_end))]
+    fig = px.line(df, x="date", y="adjusted close",hover_name="date",render_mode="svg")
+    st.plotly_chart(fig)
 
 
 # In[ ]:
@@ -84,12 +89,4 @@ df = request_stock_price_hist(ticker_symbol', key)
 
 # convert date to year and month variables
 #date_time_obj = datetime.strptime(df['date'], '%d/%m/%y %H:%M:%S')
-
-
-# In[ ]:
-
-
-# plots 
-fig = px.line(df, x="date", y="close",hover_name="date",render_mode="svg")
-fig.show()
 
